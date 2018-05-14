1f06455a8ebe00fa0770a271fca1d780b28a2c13
Zac Willmington <zac.willmington@gmail.com>
2018-05-14 01:09:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,13 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :index
+    end
+
+    post '/teams' do
+        erb :team
+    end
+
 
 end
