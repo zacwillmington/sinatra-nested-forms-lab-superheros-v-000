15fa1652c49d8f206dfcfdbb73d344b258125c00
Zac Willmington <zac.willmington@gmail.com>
2018-05-14 01:27:15 +0000

    automatically backed up by learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,6 +1,11 @@
 class Hero
 
-binding.pry
+attr_accessor :name, :power, :bio
 
+    def initialize(name, power, bio)
+        @name = name
+        @power = power
+        @bio = @bio
+    end
 
 end
