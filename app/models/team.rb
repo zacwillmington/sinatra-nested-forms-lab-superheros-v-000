9139a984c963a3f6bb9e751a53b885dd8d54a69a
Zac Willmington <zac.willmington@gmail.com>
2018-05-14 01:28:33 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,17 +2,9 @@ class Team
 
     attr_accessor :name, :motto
 
-        @@all = []
-
         def initialize(name, motto)
             @name = name
             @motto = motto
         end
-
-        def self.all
-            @@all
-        end
-
-    end
 
 end
