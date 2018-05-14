0d57f41c2779fd507df0fdfa6549460e3c2fb0e1
Zac Willmington <zac.willmington@gmail.com>
2018-05-14 01:27:42 +0000

    automatically backed up by learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -5,7 +5,11 @@ attr_accessor :name, :power, :bio
     def initialize(name, power, bio)
         @name = name
         @power = power
-        @bio = @bio
+        @bio = bio
+    end
+
+    def self.all
+        @@all
     end
 
 end
