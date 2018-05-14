3ac226d80292b7f32e247e713867e1f929b1d173
Zac Willmington <zac.willmington@gmail.com>
2018-05-14 01:35:33 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,13 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-        binding.pry
         erb :index
     end
 
     post '/teams' do
         binding.pry
         @team = Team.new(params[:team][:name], params[:team][:motto])
-        
+
         params[:team][:members].each do |hero|
             Hero.new(hero)
         end
