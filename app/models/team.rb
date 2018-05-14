e1286cb6676680443975467719c61994107ffa58
Zac Willmington <zac.willmington@gmail.com>
2018-05-14 01:28:10 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,19 @@
-class Team < ActiveRecord::Base
+class Team 
+    
+    attr_accessor :name, :motto
+
+        @@all = []
+
+        def initialize(name, power, bio)
+            @name = name
+            @power = power
+            @bio = bio
+        end
+
+        def self.all
+            @@all
+        end
+
+    end
 
 end
